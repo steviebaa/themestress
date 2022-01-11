0deda77b0d7998a527b06d11d3d6f9ed8aa22a5a
Steve Richardson <steve.richardson.dev@gmail.com>
2022-01-12 08:44:53 +1100

    started adding some tests and config

---
+++ packages/themestress/jest.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { resolve } = require('path');
+
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/config/.enzyme.ts'],
+  testEnvironment: 'jsdom',
+  transform: { '^.+\\.(t|j)sx?$': 'ts-jest' },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@core/(.*)$': resolve(__dirname, './src/core/$1'),
+    '^@components/(.*)$': resolve(__dirname, './src/components/$1'),
+    '^@icons/(.*)$': resolve(__dirname, './src/icons/$1'),
+  },
+};
---
+++ packages/themestress/src/__tests__/Text.spec.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Text } from '../components';
+
+describe('Component Text', () => {
+  it('Should render a Text component', () => {
+    const wrapper = mount(<Text>Test Text</Text>);
+
+    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant h1', () => {
+    const wrapper = mount(<Text variant="h1">Test Text</Text>);
+
+    expect(wrapper.find('h1')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant h2', () => {
+    const wrapper = mount(<Text variant="h2">Test Text</Text>);
+
+    expect(wrapper.find('h2')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant h3', () => {
+    const wrapper = mount(<Text variant="h3">Test Text</Text>);
+
+    expect(wrapper.find('h3')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant h4', () => {
+    const wrapper = mount(<Text variant="h4">Test Text</Text>);
+
+    expect(wrapper.find('h4')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant h5', () => {
+    const wrapper = mount(<Text variant="h5">Test Text</Text>);
+
+    expect(wrapper.find('h5')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant h6', () => {
+    const wrapper = mount(<Text variant="h6">Test Text</Text>);
+
+    expect(wrapper.find('h6')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant subtitle1', () => {
+    const wrapper = mount(<Text variant="subtitle1">Test Text</Text>);
+
+    expect(wrapper.find('h6')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant subtitle2', () => {
+    const wrapper = mount(<Text variant="subtitle2">Test Text</Text>);
+
+    expect(wrapper.find('h6')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant body1', () => {
+    const wrapper = mount(<Text variant="body1">Test Text</Text>);
+
+    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant body2', () => {
+    const wrapper = mount(<Text variant="body2">Test Text</Text>);
+
+    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant button', () => {
+    const wrapper = mount(<Text variant="button">Test Text</Text>);
+
+    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant caption', () => {
+    const wrapper = mount(<Text variant="caption">Test Text</Text>);
+
+    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant overline', () => {
+    const wrapper = mount(<Text variant="overline">Test Text</Text>);
+
+    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant code', () => {
+    const wrapper = mount(<Text variant="code">Test Text</Text>);
+
+    expect(wrapper.find('code')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+
+  it('Should render a Text component with variant inherit', () => {
+    const wrapper = mount(<Text variant="inherit">Test Text</Text>);
+
+    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.text()).toContain('Test Text');
+  });
+});
