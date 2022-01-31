b2feecdb960c5f4c391d6723a2f374bf761779b8
Steve Richardson <steve.richardson.dev@gmail.com>
2022-02-01 07:36:56 +1100

    #20 Added ThemeZIndices class

---
+++ src/themestress/core/classes/ThemeZIndices.spec.ts
@@ -0,0 +1,26 @@
+import {ThemeZIndices} from './ThemeZIndices';
+
+describe('Class ThemeZIndices', () => {
+  it('should set the default props', () => {
+    const zIndices = new ThemeZIndices();
+    expect(zIndices.navbar).toEqual(1100);
+    expect(zIndices.backdrop).toEqual(1300);
+    expect(zIndices.modal).toEqual(1400);
+    expect(zIndices.snackbar).toEqual(1500);
+    expect(zIndices.tooltip).toEqual(1600);
+  });
+  it('should set the custom props', () => {
+    const zIndices = new ThemeZIndices({
+      navbar: 1,
+      backdrop: 2,
+      modal: 3,
+      snackbar: 4,
+      tooltip: 5,
+    });
+    expect(zIndices.navbar).toEqual(1);
+    expect(zIndices.backdrop).toEqual(2);
+    expect(zIndices.modal).toEqual(3);
+    expect(zIndices.snackbar).toEqual(4);
+    expect(zIndices.tooltip).toEqual(5);
+  });
+});
---
+++ src/themestress/core/classes/ThemeZIndices.ts
@@ -0,0 +1,35 @@
+interface ThemeZIndicesInitializer {
+  navbar?: number;
+  backdrop?: number;
+  modal?: number;
+  snackbar?: number;
+  tooltip?: number;
+}
+
+interface ThemeZIndicesProps {
+  navbar?: number;
+  backdrop?: number;
+  modal?: number;
+  snackbar?: number;
+  tooltip?: number;
+}
+
+export class ThemeZIndices implements ThemeZIndicesProps {
+  public navbar: number;
+  public backdrop: number;
+  public modal: number;
+  public snackbar: number;
+  public tooltip: number;
+
+  constructor(zIndices?: ThemeZIndicesInitializer) {
+    this._setProperties(zIndices);
+  }
+
+  private _setProperties(zIndices?: ThemeZIndicesInitializer) {
+    this.navbar = zIndices?.navbar ?? 1100;
+    this.backdrop = zIndices?.backdrop ?? 1300;
+    this.modal = zIndices?.modal ?? 1400;
+    this.snackbar = zIndices?.snackbar ?? 1500;
+    this.tooltip = zIndices?.tooltip ?? 1600;
+  }
+}
