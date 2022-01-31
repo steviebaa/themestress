6a1ff5a911b34a6eb34233edce9a59191771903a
Steve Richardson <steve.richardson.dev@gmail.com>
2022-01-31 18:16:01 +1100

    added neutralvariant class and tests

---
+++ src/themestress/core/classes/NeutralVariantPalette.ts
@@ -0,0 +1,32 @@
+import {ThemeMode} from '..';
+import {Color} from './base/Color';
+import {TonalPalette} from './base/TonalPalette';
+
+export class NeutralVariantPalette extends TonalPalette {
+  private _outlineTone = 50;
+  private _surfaceTone: number;
+
+  constructor(props: Color, mode: ThemeMode = 'light') {
+    super(props);
+
+    this.setTones(mode);
+  }
+
+  /** Get the outline color */
+  get outline(): Color {
+    return this.tones[this._outlineTone];
+  }
+
+  /** Get the surface color */
+  get surface(): Color {
+    return this.tones[this._surfaceTone];
+  }
+
+  /** Set the tones of the palette to the recommend values */
+  public setTones(mode: ThemeMode) {
+    const isLight = mode === 'light';
+
+    this._surfaceTone = isLight ? 90 : 10;
+    this.surface.on = this.tones[isLight ? 30 : 70];
+  }
+}
