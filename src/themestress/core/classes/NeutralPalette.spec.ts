850cd7244d9ead214c016ee23cc1fb062e229ede
Steve Richardson <steve.richardson.dev@gmail.com>
2022-01-31 18:01:27 +1100

    added neutralpalette and tests

---
+++ src/themestress/core/classes/NeutralPalette.spec.ts
@@ -0,0 +1,28 @@
+import {NeutralPalette} from './NeutralPalette';
+import {Color} from './base/Color';
+
+describe('Class NeutralPalette', () => {
+  it('should set the light and dark mode tones', () => {
+    const lightPalette = new NeutralPalette(new Color('#aaaaaa'));
+    expect(lightPalette['_backgroundTone']).toEqual(99);
+    expect(lightPalette['_surfaceTone']).toEqual(99);
+
+    const darkPalette = new NeutralPalette(new Color('#aaaaaa'), 'dark');
+    expect(darkPalette['_backgroundTone']).toEqual(10);
+    expect(darkPalette['_surfaceTone']).toEqual(10);
+  });
+  it('should return the correct background color', () => {
+    const palette = new NeutralPalette(new Color('#aaaaaa'));
+    expect(palette.background.hex).toEqual('#fcfcfc');
+
+    palette.setTones('dark');
+    expect(palette.background.hex).toEqual('#1a1a1a');
+  });
+  it('should return the correct surface color', () => {
+    const palette = new NeutralPalette(new Color('#aaaaaa'));
+    expect(palette.surface.hex).toEqual('#fcfcfc');
+
+    palette.setTones('dark');
+    expect(palette.surface.hex).toEqual('#1a1a1a');
+  });
+});
---
+++ src/themestress/core/classes/NeutralPalette.ts
@@ -0,0 +1,35 @@
+import {ThemeMode} from '..';
+import {Color} from './base/Color';
+import {TonalPalette} from './base/TonalPalette';
+
+export class NeutralPalette extends TonalPalette {
+  private _backgroundTone: number;
+  private _surfaceTone: number;
+
+  constructor(props: Color, mode: ThemeMode = 'light') {
+    super(props);
+
+    this.setTones(mode);
+  }
+
+  /** Get the background color */
+  get background(): Color {
+    return this.tones[this._backgroundTone];
+  }
+
+  /** Get the surface color */
+  get surface(): Color {
+    return this.tones[this._surfaceTone];
+  }
+
+  /** Set the tones of the palette to the recommend values */
+  public setTones(mode: ThemeMode) {
+    const isLight = mode === 'light';
+
+    this._backgroundTone = isLight ? 99 : 10;
+    this.background.on = this.tones[isLight ? 10 : 99];
+
+    this._surfaceTone = isLight ? 99 : 10;
+    this.surface.on = this.tones[isLight ? 10 : 99];
+  }
+}
